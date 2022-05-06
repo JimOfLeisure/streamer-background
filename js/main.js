24e06c813937c6dacec4759e2845305f6661bd06
Jim Nelson <jim@jimnelson.us>
2022-05-05 20:32:37 -0700

    stubs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('hi');
+
+class Vec2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class BackgroundProp {
+    constructor(pos) {
+        this.pos = pos;
+    }
+}
+
+const thing = new BackgroundProp(new Vec2(50,50));
+console.log(thing.pos.x);
+console.log(thing.pos.y);
