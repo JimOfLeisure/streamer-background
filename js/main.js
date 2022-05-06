da9f5f04210838ec36dbbc041e7c298248e29d62
Jim Nelson <jim@jimnelson.us>
2022-05-05 20:41:08 -0700

    streamerBG & adding props

--- js/main.js
+++ js/main.js
@@ -13,6 +13,24 @@ class BackgroundProp {
     }
 }
 
+class StreamerBackground {
+    constructor(canvas, props = []) {
+        this.canvas = canvas;
+        this.props = props;
+    }
+    addProp(prop) {
+        this.props.push(prop);
+    }
+}
+
+const canvas = document.querySelector('#canvas');
+
 const thing = new BackgroundProp(new Vec2(50,50));
 console.log(thing.pos.x);
 console.log(thing.pos.y);
+
+const streamerBG = new StreamerBackground(canvas);
+console.log(streamerBG.canvas);
+
+streamerBG.addProp(thing);
+console.log(streamerBG.props);
