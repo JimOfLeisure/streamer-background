e09b343e6c47c80f758aa32cc1b0f3131b616bf1
Jim Nelson <jim@jimnelson.us>
2022-05-05 20:41:54 -0700

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log('hi');
-
 class Vec2 {
     constructor(x, y) {
         this.x = x;
@@ -26,11 +24,7 @@ class StreamerBackground {
 const canvas = document.querySelector('#canvas');
 
 const thing = new BackgroundProp(new Vec2(50,50));
-console.log(thing.pos.x);
-console.log(thing.pos.y);
 
 const streamerBG = new StreamerBackground(canvas);
-console.log(streamerBG.canvas);
 
 streamerBG.addProp(thing);
-console.log(streamerBG.props);
